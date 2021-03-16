aff7068d877e7900b7682707ad22e7c2e84df895
gregnottage <64895572+gregnottage@users.noreply.github.com>
2021-03-16 08:58:59 +0000

    Update Invoke-Upload.ps1
    Fixed to allow passthrough of username

--- Upload-IntuneWin/Invoke-Upload.ps1
+++ Upload-IntuneWin/Invoke-Upload.ps1
@@ -7,8 +7,15 @@ See LICENSE in the project root for license information.
 #>
 #Script to initiate the Upload-IntuneWin script process
 Param(
-    [parameter(Mandatory = $true, HelpMessage = "Enter a name for the new package")]
+    [Parameter(Mandatory = $true, Position = 1,
+        HelpMessage = 'Please specify an Azure/Intune admin user name'
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string] $userName,
+    
+    [parameter(Mandatory = $true, Position = 2,
+        HelpMessage = "Enter a name for the new package")]
     $Name
 )
 
-Powershell -ExecutionPolicy Bypass -file "$PSScriptRoot\Upload-IntuneWin.ps1" -packagePath "$PSScriptRoot\$Name" -intuneWinAppUtilPath $PSScriptRoot
+Powershell -ExecutionPolicy Bypass -file "$PSScriptRoot\Upload-IntuneWin.ps1" -userName $userName -packagePath "$PSScriptRoot\$Name" -intuneWinAppUtilPath $PSScriptRoot
